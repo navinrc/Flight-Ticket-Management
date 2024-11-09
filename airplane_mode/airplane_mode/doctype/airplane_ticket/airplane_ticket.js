eaf4da6c1f02da3496aebcf97be25a9ee0ff0343
navinrc <navinrc98@gmail.com>
2024-11-09 14:06:57 +0530

    add ons in flight ticket and validations

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,29 @@
 // Copyright (c) 2024, Navin R C and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+	refresh(frm) {},
+	flight_price(frm) {
+		frm.trigger("update_total_amount");
+	},
+	update_total_amount(frm) {
+		let items_price = 0;
+		for (let item of frm.doc.add_ons) {
+			items_price += item.amount;
+		}
 
-// 	},
-// });
+		const amount = frm.doc.flight_price + items_price;
+		frm.set_value("total_amount", amount);
+	},
+});
+
+frappe.ui.form.on("Airplane Ticket Add-on Item", {
+	amount(frm) {
+		// trigger fn
+		frm.trigger("update_total_amount");
+	},
+	add_ons_remove(frm) {
+
+		frm.trigger("update_total_amount");
+	}
+});
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -1,9 +1,29 @@
 # Copyright (c) 2024, Navin R C and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
+from frappe import _
 
 
 class AirplaneTicket(Document):
-	pass
+	def validate(self):
+		self.remove_duplicates()
+
+	#!hate this logic as its removing rows without user intimation, recheck on submission
+	def remove_duplicates(self):
+		unique_values = set()
+		unique_rows = []
+
+		for row in self.add_ons:
+			if row.item not in unique_values:
+				unique_values.add(row.item)
+				unique_rows.append(row)
+
+		self.add_ons = unique_rows
+
+	def before_submit(self):
+		# Check if the status is "Boarded"
+		if self.status != "Boarded":
+			# Raise an error to prevent submission
+			frappe.throw(_("Cannot submit the document. Status must be 'Boarded' to submit."))

d5979c50f59690670dda52a817fc40fea67a3943
navinrc <navinrc98@gmail.com>
2024-11-11 23:03:37 +0530

    fixes in controller logic

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -3,27 +3,27 @@
 
 frappe.ui.form.on("Airplane Ticket", {
 	refresh(frm) {},
-	flight_price(frm) {
-		frm.trigger("update_total_amount");
-	},
-	update_total_amount(frm) {
-		let items_price = 0;
-		for (let item of frm.doc.add_ons) {
-			items_price += item.amount;
-		}
+// 	flight_price(frm) {
+// 		frm.trigger("update_total_amount");
+// 	},
+// 	update_total_amount(frm) {
+// 		let items_price = 0;
+// 		for (let item of frm.doc.add_ons) {
+// 			items_price += item.amount;
+// 		}
 
-		const amount = frm.doc.flight_price + items_price;
-		frm.set_value("total_amount", amount);
-	},
-});
+// 		const amount = frm.doc.flight_price + items_price;
+// 		frm.set_value("total_amount", amount);
+// 	},
+// });
 
-frappe.ui.form.on("Airplane Ticket Add-on Item", {
-	amount(frm) {
-		// trigger fn
-		frm.trigger("update_total_amount");
-	},
-	add_ons_remove(frm) {
+// frappe.ui.form.on("Airplane Ticket Add-on Item", {
+// 	amount(frm) {
+// 		// trigger fn
+// 		frm.trigger("update_total_amount");
+// 	},
+// 	add_ons_remove(frm) {
 
-		frm.trigger("update_total_amount");
-	}
+// 		frm.trigger("update_total_amount");
+// 	}
 });
--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.py
@@ -6,24 +6,26 @@ from frappe.model.document import Document
 from frappe import _
 import random
 
-
+# airplane_ticket.py
 class AirplaneTicket(Document):
+	def calculate_total_amount(self):
+		# Calculate total by adding Flight Price and all add-ons' Amounts
+		total_add_ons = sum(add_on.amount for add_on in self.add_ons)
+		self.total_amount = self.flight_price + total_add_ons
+
+	def remove_duplicate_add_ons(self):
+		# Remove duplicate add-ons based on the 'Item' field
+		unique_add_ons = {}
+		for add_on in self.add_ons:
+			if add_on.item not in unique_add_ons:
+				unique_add_ons[add_on.item] = add_on
+		self.add_ons = list(unique_add_ons.values())
+
 	def validate(self):
-		self.remove_duplicates()
+		self.calculate_total_amount()
+		self.remove_duplicate_add_ons()
 
-	#!hate this logic as its removing rows without user intimation, recheck on submission
-	def remove_duplicates(self):
-		unique_values = set()
-		unique_rows = []
-
-		for row in self.add_ons:
-			if row.item not in unique_values:
-				unique_values.add(row.item)
-				unique_rows.append(row)
-
-		self.add_ons = unique_rows
-
-	# <random-integer><random-capital-alphabet-from-A-to-E> - 89E, 21A
+	# Generate seat number in the format "<random-integer><random-capital-letter-from-A-to-E>"
 	def generate_seat_number(self):
 		# Generate a random integer
 		random_integer = random.randint(0, 9)
@@ -42,4 +44,3 @@ class AirplaneTicket(Document):
 
 	def before_insert(self):
 		self.generate_seat_number()
-
